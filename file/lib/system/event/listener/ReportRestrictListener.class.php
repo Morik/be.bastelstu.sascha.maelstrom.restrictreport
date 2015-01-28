b43c1fae5880abff6205d8e67e858d0958c509fb
Morik <Sebastian.Zimmer@gmx.net>
2015-01-28 21:06:01 +0100

    Update ReportRestrictListener.class.php

--- file/lib/system/event/listener/ReportRestrictListener.class.php
+++ file/lib/system/event/listener/ReportRestrictListener.class.php
@@ -1,14 +1,15 @@
 <?php
 namespace wcf\system\event\listener;
+use wcf\data\moderation\queue\ModerationQueueList;
 use wcf\system\event\IEventListener;
 use wcf\system\exception\AJAXException;
+use wcf\system\exception\NamedUserException;
 use wcf\system\exception\PermissionDeniedException;
 use wcf\system\WCF;
-
 /**
  * Restricts the ability to report contents.
  * 
- * @author	Sascha Greuel
+ * @author	Sascha Greuel, Sebastian Zimmer
  * @copyright	2014 Sascha Greuel
  * @license	Creative Commons Attribution-NoDerivatives <http://creativecommons.org/licenses/by-nd/4.0/legalcode>
  * @package	be.bastelstu.sascha.maelstrom.restrictreport
@@ -22,16 +23,29 @@ class ReportRestrictListener implements IEventListener {
 	public function execute($eventObj, $className, $eventName) {
 		$actionName = $eventObj->getActionName();
 		
-		if (!WCF::getUser()->userID || WCF::getUser()->banned || WCF::getUser()->activationCode || WCF::getUser()->reactivationCode || !WCF::getSession()->getPermission('user.community.canReport')) {
-			if ($actionName == 'prepareReport' || $actionName == 'report') {
+		if ($actionName == 'prepareReport' || $actionName == 'report') {
+			if (!WCF::getUser()->userID || WCF::getUser()->banned || WCF::getUser()->activationCode || WCF::getUser()->reactivationCode || !WCF::getSession()->getPermission('user.community.canReport')) {
 				if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
 					throw new AJAXException(WCF::getLanguage()->get('wcf.ajax.error.permissionDenied'), AJAXException::INSUFFICIENT_PERMISSIONS);
 				}
 				else {
 					throw new PermissionDeniedException();
 				}
-					
 				exit;
+			}
+			elseif(WCF::getSession()->getPermission('user.community.reportLimit') != -1){
+				$moderationQueueList = new ModerationQueueList();
+				$moderationQueueList->getConditionBuilder()->add("userID = ?", array(WCF::getUser()->userID));
+				$moderationQueueList->getConditionBuilder()->add("time > ?", array((TIME_NOW-(24*60*60))));
+				if($moderationQueueList->countObjects() > WCF::getSession()->getPermission('user.community.reportLimit')){
+				if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
+					throw new AJAXException('<p class="error">'.WCF::getLanguage()->get('wcf.global.error.reportLimitReached').'</p>', AJAXException::INSUFFICIENT_PERMISSIONS);
+				}
+				else {
+					throw new NamedUserException(WCF::getLanguage()->get('wcf.global.error.reportLimitReached'));
+				}
+				exit;
+				}
 			}
 		}
 	}
